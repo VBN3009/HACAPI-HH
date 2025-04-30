0aa89113d374308b40dbec4e635a698514499371
VBN3009 <144623629+VBN3009@users.noreply.github.com>
2025-04-29 21:45:12 -0500

    Initial commit of HallHop Chrome Extension

--- extensions.py
+++ extensions.py
@@ -1,11 +1,11 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_jwt_extended import JWTManager
+import os
 
 limiter = Limiter(
     key_func=get_remote_address,
-    storage_uri="memory://",
-    storage_options={"socket_connect_timeout": 30},
+    storage_uri=os.getenv("REDIS_URL", "memory://"),
     strategy="fixed-window",
 )
 
