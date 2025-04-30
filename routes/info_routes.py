af074a67b29af5d497daef1fd04eefa87c3199cf
VBN3009 <144623629+VBN3009@users.noreply.github.com>
2025-04-29 22:13:10 -0500

    Initial commit of HallHop Chrome Extension

--- routes/info_routes.py
+++ routes/info_routes.py
@@ -7,8 +7,11 @@ info_bp = Blueprint("info", __name__)
 @info_bp.route("/api/getInfo", methods=["POST"])
 @jwt_required()
 def get_info():
-    creds = get_jwt_identity()
-    session = HACSession(creds["username"], creds["password"], creds["base_url"])
-    session.login()
+    identity = get_jwt_identity()  # pulls from the token
+    user = identity.get("username")
+    password = identity.get("password")
+    base_url = identity.get("base_url")
+
+    session = HACSession(user, password, base_url)
     data = session.get_info()
     return jsonify(data)
